32adb66c92d6218db7a4145bdb4481292cc192b4
Darligworld <34596680+Darligworld@users.noreply.github.com>
2018-05-09 22:41:24 +0100

    Update and rename Example.js to Agrostore.js

---
+++ app/components/Agrostore.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Agrostore.css';
+
+class Agrostor extends React.Component {
+    render() {
+        return (
+            <div>
+                <Title />
+            </div>
+        )
+    }
+}
+
+export default Agrostore;
