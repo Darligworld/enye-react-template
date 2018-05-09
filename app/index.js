3a2c17215d79c0822d97744bf58bc817104c8305
Darligworld <34596680+Darligworld@users.noreply.github.com>
2018-05-09 22:48:45 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './components/Example';
+import Example from './components/Agrostore';
 
 const root = document.querySelector('main');
 
-ReactDOM.render(<Example/>, root)
+ReactDOM.render(<Agrostore />, root)
