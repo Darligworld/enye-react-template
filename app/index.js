abb3dc7cb1721ff35a014c3b860b18c7c249d1fc
Darligworld <sunset4darlington@gmail.com>
2018-06-06 19:14:13 +0100

    my agrostore app

--- app/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './components/Agrostore';
-
-const root = document.querySelector('main');
-
-ReactDOM.render(<Agrostore />, root)
